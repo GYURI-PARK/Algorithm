f77d45070558144657c40be99f342834d907d3db
Doris <93391058+GYURI-PARK@users.noreply.github.com>
2023-06-12 18:39:26 +0900

    add: BOJ_2493_탑

--- BOJ_Python/자료구조_스택/BOJ_2493_탑.py
+++ BOJ_Python/자료구조_스택/BOJ_2493_탑.py
@@ -1,3 +1,25 @@
 ## 자료구조 스택
 ### BOJ_2493_탑
 
+import sys
+
+n = int(sys.stdin.readline()) 
+
+tower = list(map(int, sys.stdin.readline().split()))
+stack = []
+result = []
+
+for i in range(n):
+    while stack:
+        if stack[-1][1] >= tower[i]:
+            result.append(stack[-1][0]+1) 
+            break
+        else: stack.pop()
+
+    if not stack:
+        result.append(0) 
+    
+    stack.append((i, tower[i])) 
+
+for i in result:
+    print(i, end = ' ')
