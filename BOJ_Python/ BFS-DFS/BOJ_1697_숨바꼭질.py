38dbe9060307044207c107b20e5c6dbac4216f24
Doris <93391058+GYURI-PARK@users.noreply.github.com>
2023-07-10 02:58:14 +0900

    Update BOJ_1697_숨바꼭질.py

--- BOJ_Python/ BFS-DFS/BOJ_1697_숨바꼭질.py
+++ BOJ_Python/ BFS-DFS/BOJ_1697_숨바꼭질.py
@@ -4,17 +4,18 @@ n, k = map(int, input().split())
 
 def bfs(start, end):
     q = deque()
-    visited = [0] * (100001)
+    visited = [-1] * (100001)
 
     q.append(start)
+    visited[start] = 0
     while q:
         x = q.popleft()
 
         if x == end:
             return visited[x]
         
-        for i in (x-1, x+1, 2*x) :
-            if 0 <= i <= 100000 and not visited[i]:
+        for i in (x-1, 2*x, x+1) :
+            if 0 <= i <= 100000 and visited[i] == -1:
                 q.append(i)
                 visited[i] = visited[x] + 1
 
