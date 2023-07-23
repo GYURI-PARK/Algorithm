9585d19c564609123dc1f319fcd88f95e2ae11e4
Doris <93391058+GYURI-PARK@users.noreply.github.com>
2023-07-23 23:06:03 +0900

    add: 다익스트라

--- BOJ_Python/DP/BOJ_1446_지름길.py
+++ BOJ_Python/DP/BOJ_1446_지름길.py
@@ -7,11 +7,13 @@ dis = [i for i in range(d+1)]
 for i in range(d+1):
 
     # 지름길로 간 거리와 고속도로로 간 거리를 비교
+    # 한칸 전 위치의 테이블 값 + 1이 현재 테이블 값보다 작다면 현재 테이블 값을 한칸전 위치의 테이블 + 1로 바꾼다.
     dis[i] = min(dis[i], dis[i-1]+1)
 
     # 지름길을 반복하여 최단 거리를 찾는다.
     for start, end, short in graph:
         if i == start and end <= d and dis[i] + short < dis[end]:
+            # 현재 위치에 지름길이 있다면 (지름길로 건너간 곳의 원래 테이블 값)과 (지름길로 건너가기 전의 테이블 값+지름길의 거리) 중 더 작은 값으로 건너간 곳의 값을 바꾼다. 
             dis[end] = dis[i] + short
 
 # 고속도로의 끝에 도착했을 때까지 걸린 거리를 출력
--- BOJ_Python/Dijkstra/BOJ_1238_파티.py
+++ BOJ_Python/Dijkstra/BOJ_1238_파티.py
@@ -18,22 +18,29 @@ def dijkstra(start):
 
     while pq:
         dist, now = heapq.heappop(pq)
+        # 기존 최단거리보다 멀 경우, 무시
         if distance[now] < dist:
             continue
 
+        # 인접노드 탐색
         for node_index, node_cost in graph[now]:
             cost = dist + node_cost
 
+            # 기존 거리보다 짧을 경우
             if distance[node_index] > cost:
                 distance[node_index] = cost
-                heapq.heappush(pq, (cost, node_index))
-
+                heapq.heappush(pq, (cost, node_index))      
     return distance
 
 res = 0
 for i in range(1, n+1):
-    go = dijkstra(i)
-    back = dijkstra(x)
+    # 각 마을을 출발지점으로 했을 때의 최단 거리를 나타내는 리스트
+    go = dijkstra(i) 
+    
+    # 출발지점 x를 기준으로 각 마을까지의 최단 거리를 나타내는 리스트
+    back = dijkstra(x) 
+    
+     # 모든 마을을 출발지점으로 했을 때와 출발지점 x를 기준으로 각 마을까지의 거리의 합을 구하여 그 중 가장 큰 값을 res에 저장
     res = max(res, go[x] + back[i])
 
 print(res)
