8cc91a8f59ad13330038af0d5b90258a51552887
Doris <93391058+GYURI-PARK@users.noreply.github.com>
2023-08-19 23:25:18 +0900

    add: BOJ_3079

---
+++ BOJ_Python/이분탐색/BOJ_3079_입국심사.py
@@ -0,0 +1,25 @@
+import sys
+n, m = map(int, sys.stdin.readline().split())
+t = []
+for i in range(n):
+    t.append(int(sys.stdin.readline()))
+
+start = min(t)
+end = max(t) * m
+res = end
+
+while start <= end:
+    total = 0
+    # mid시간 동안 검사할 수 있는 총 사람 수
+    mid = (start + end) // 2
+    for i in range(n):
+        total += mid // t[i]
+       
+    print('total,', total)
+    if total >= m:
+        end = mid - 1
+        res = min(res, mid)
+    else:
+        start = mid + 1
+
+print(res)
