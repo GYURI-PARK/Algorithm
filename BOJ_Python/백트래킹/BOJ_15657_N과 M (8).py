db54716bd1c46c71317c92b8dc46af1cdfdf0174
Doris <93391058+GYURI-PARK@users.noreply.github.com>
2023-09-18 00:34:58 +0900

    add: BOJ_15657

---
+++ BOJ_Python/백트래킹/BOJ_15657_N과 M (8).py
@@ -0,0 +1,17 @@
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+li = list(map(int, sys.stdin.readline().split()))
+box = []
+
+li.sort()
+
+def backtracking(depth):
+    if len(box) == m:
+        print(' '.join(map(str, box)))
+        return
+    for i in range(depth, n):
+        box.append(li[i])  
+        backtracking(i)
+        box.pop()
+backtracking(0)
