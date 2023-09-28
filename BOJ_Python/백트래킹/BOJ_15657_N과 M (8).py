ed321bf27c7dafffbb750ca35bfe6769eeb16400
Doris <93391058+GYURI-PARK@users.noreply.github.com>
2023-09-28 21:09:08 +0900

    add: BOJ_9465

--- BOJ_Python/백트래킹/BOJ_15657_N과 M (8).py
+++ BOJ_Python/백트래킹/BOJ_15657_N과 M (8).py
@@ -4,15 +4,14 @@ n, m = map(int, sys.stdin.readline().split())
 li = list(map(int, sys.stdin.readline().split()))
 box = []
 
-a = list(set(li))
-b = sorted(a)
+li.sort()
 
 def backtracking(depth):
     if len(box) == m:
         print(' '.join(map(str, box)))
         return
-    for i in range(depth, len(b)):
-        box.append(b[i])  
+    for i in range(depth, n):
+        box.append(li[i])  
         backtracking(i)
         box.pop()
 backtracking(0)
--- BOJ_Python/백트래킹/BOJ_15666_N과 M (12).py
+++ BOJ_Python/백트래킹/BOJ_15666_N과 M (12).py
@@ -2,22 +2,17 @@ import sys
 
 n, m = map(int, sys.stdin.readline().split())
 li = list(map(int, sys.stdin.readline().split()))
-visited = [False] * n
 box = []
 
-li.sort()
+a = list(set(li))
+b = sorted(a)
 
 def backtracking(depth):
-    if depth == m:
+    if len(box) == m:
         print(' '.join(map(str, box)))
         return
-    tmp = 0
-    for i in range(n):
-        if not visited[i] and tmp != li[i]:
-            visited[i] = True
-            box.append(li[i])   
-            tmp = li[i]
-            backtracking(depth + 1)
-            visited[i] = False
-            box.pop()
+    for i in range(depth, len(b)):
+        box.append(b[i])  
+        backtracking(i)
+        box.pop()
 backtracking(0)
