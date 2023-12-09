4ef920f1b461b4e9f8a3ef41882346fb3fcf6f76
GYURI-PARK <93391058+GYURI-PARK@users.noreply.github.com>
2023-12-09 22:27:51 +0900

    add: BOJ_11727_2xn 타일링2

---
+++ BOJ_Swift/DP/BOJ_11727_2xn 타일링 2.swift
@@ -0,0 +1,32 @@
+let n = Int(readLine()!)!
+var dp = [Int](repeating: 0, count: n+1)
+
+if n == 1 {
+    print(1)
+} else if n == 2 {
+    print(3)
+} else {
+    dp[1] = 1
+    dp[2] = 3
+    
+    for i in 3..<n+1 {
+        dp[i] = (dp[i-1] + 2*dp[i-2]) % 10007
+    }
+    print(dp[n])
+}
+
+let n = Int(readLine()!)!
+var dp = [Int](repeating: 0, count: 1001)
+
+dp[1] = 1
+dp[2] = 3
+
+if n < 3 {
+    print(dp[n])
+} else {
+    for i in 3...n {
+        dp[i] = ((dp[i - 1] % 10007) + (2 * dp[i - 2] % 10007)) % 10007
+    }
+    
+    print(dp[n])
+}
